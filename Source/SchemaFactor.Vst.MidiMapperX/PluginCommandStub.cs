8c1202a45ede88d5a1be93361569da6cd0ddbb56
Leif Bloomquist <github@leifbloomquist.net>
2012-12-04 19:29:27 -0500

    Added GetMidiKeyName() and 64-bit Binaries
    Main binary hasn't been rebuilt yet though!

--- Source/SchemaFactor.Vst.MidiMapperX/PluginCommandStub.cs
+++ Source/SchemaFactor.Vst.MidiMapperX/PluginCommandStub.cs
@@ -1,4 +1,4 @@
-﻿namespace SchemaFactor.Vst.MidiMapperX
+namespace SchemaFactor.Vst.MidiMapperX
 {
     using Jacobi.Vst.Core.Plugin;
     using Jacobi.Vst.Framework;
@@ -9,13 +9,33 @@
     /// </summary>
     public class PluginCommandStub : StdPluginDeprecatedCommandStub, IVstPluginCommandStub
     {
+        Plugin p = new Plugin();
+
         /// <summary>
         /// Called by the framework to create the plugin root class.
         /// </summary>
         /// <returns>Never returns null.</returns>
         protected override IVstPlugin CreatePluginInstance()
         {
-            return new Plugin();
+            return p;
+        }
+
+        /// <summary>
+        /// Called by the host?  Returns MIDI Key name, so it is displayed i.e. in the FL Studio Piano Roll.
+        /// </summary>
+        /// <returns>True if the note map is defined, false otherwise</returns>
+        public override bool GetMidiKeyName(Jacobi.Vst.Core.VstMidiKeyName midiKeyName, int channel)
+        {
+            if (p.NoteMap.Contains((byte)midiKeyName.CurrentKeyNumber))
+            {
+                midiKeyName.Name = p.NoteMap[(byte)midiKeyName.CurrentKeyNumber].KeyName;
+                return true;
+            }
+            else
+            {
+                midiKeyName.Name = "Note Map #" + midiKeyName.CurrentKeyNumber;   // This is actually never seen since we return false below
+                return false;
+            }
         }
     }
 }
